36e314e7d162653847aa3b461571e2d80996172e
Dosugamea@SourceTree <17107514+Dosugamea@users.noreply.github.com>
2020-04-03 18:04:56 +0900

    LINEログイン/連携の実装 と 各種ページの実装

---
+++ tester/accounts.py
@@ -0,0 +1,29 @@
+from base import BaseClient
+import json
+cl = BaseClient()
+
+# アカウント作成
+addEndpoint = "/accounts"
+params = {
+    "displayID":"omado",
+    "username":"お窓",
+    "password":"omadosandesuyo",
+    "inviteCode":"PYONPYON"
+}
+resp = cl.post(addEndpoint, json=params).json()
+print(resp)
+
+# ログイン
+postEndpoint = "/accounts/login/form"
+params = {
+    "id":"omado",
+    "password":"omadosandesuyo",
+}
+resp = cl.post(postEndpoint, json=params).json()
+print(resp)
+apiKey = resp["apiKey"]
+
+# 自分のアカウント情報
+getEndpoint = "/accounts/me"
+resp = cl.get(getEndpoint).json()
+print(resp)
